1dcc63c8acf95d5daed5668c64f5dd19f15b1046
G Seaton <greg.seaton@sierralogic.com>
2017-07-30 13:58:53 -0700

    added lein plugin codox

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.5.0"] ; json handling
                  [com.cognitect/transit-clj "0.8.300"]
-                 [io.forward/yaml "1.0.5"]])
+                 [io.forward/yaml "1.0.5"]]
+  :plugins [[lein-codox "0.10.3"]])
